c99c3f52575818c610a38a6b4c831f8b428b4759
topdude15 <topdude15@gmail.com>
2019-03-12 15:42:31 -0400

    Added settings changes to ball selection

--- Assets/Scripts/Bowling/BowlingColorLoader.cs
+++ Assets/Scripts/Bowling/BowlingColorLoader.cs
@@ -4,41 +4,58 @@ using UnityEngine;
 using UnityEngine.XR.MagicLeap;
 
 public class BowlingColorLoader : MonoBehaviour {
-
-	public static void GetBallColor(RaycastHit rayHit, MLInputController controller, GameObject ballMenu, bool ballMenuOpened, bool holdingBallMenu, GameObject bowlingBall, Material[] ballMats) {
+	public static void GetBallColor (RaycastHit rayHit, MLInputController controller, GameObject ballMenu, bool ballMenuOpened, bool holdingBallMenu, GameObject bowlingBall, Material[] ballMats) {
+		float targetSize = 0.05f;
+		float sizeChange = 0.002f;
 		if (rayHit.transform.gameObject.name == "0 RedBall") {
-					if (controller.TriggerValue >= 0.9f) {
-						PlayerPrefs.SetInt ("ballColorInt", 0);
-						ballMenu.SetActive (false);
-						ballMenuOpened = false;
-						holdingBallMenu = true;
-						LoadBallColor(bowlingBall, ballMats);
-					}
-				} else if (rayHit.transform.gameObject.name == "1 OrangeBall" && controller.TriggerValue >= 0.9f) {
-					PlayerPrefs.SetInt ("ballColorInt", 1);
-					ballMenu.SetActive (false);
-					ballMenuOpened = false;
-					holdingBallMenu = true;
-					LoadBallColor(bowlingBall, ballMats);
-				} else if (rayHit.transform.gameObject.name == "2 YellowBall" && controller.TriggerValue >= 0.9f) {
-					PlayerPrefs.SetInt ("ballColorInt", 2);
-					ballMenu.SetActive (false);
-					ballMenuOpened = false;
-					holdingBallMenu = true;
-					LoadBallColor(bowlingBall, ballMats);
-				} else if (rayHit.transform.gameObject.name == "3 GreenBall" && controller.TriggerValue >= 0.9f) {
-					PlayerPrefs.SetInt ("ballColorInt", 3);
-					ballMenu.SetActive (false);
-					ballMenuOpened = false;
-					holdingBallMenu = true;
-					LoadBallColor(bowlingBall, ballMats);
-				} else if (rayHit.transform.gameObject.name == "4 BlueBall" && controller.TriggerValue >= 0.9f) {
-					PlayerPrefs.SetInt ("ballColorInt", 4);
-					ballMenu.SetActive (false);
-					ballMenuOpened = false;
-					holdingBallMenu = true;
-					LoadBallColor(bowlingBall, ballMats);
-				}
+			print (rayHit.transform.gameObject.name);
+
+			GameObject ball = GameObject.Find ("0 RedBall");
+			//Vector3 ballSize = ball.transform.localScale;
+
+			print (ball.transform.localScale.x);
+			
+
+			if (controller.TriggerValue >= 0.9f) {
+				PlayerPrefs.SetInt ("ballColorInt", 0);
+				ballMenu.SetActive (false);
+				ballMenuOpened = false;
+				holdingBallMenu = true;
+				LoadBallColor (bowlingBall, ballMats);
+			}
+		} else if (rayHit.transform.gameObject.name == "1 OrangeBall") {
+			if (controller.TriggerValue >= 0.9f) {
+				PlayerPrefs.SetInt ("ballColorInt", 1);
+				ballMenu.SetActive (false);
+				ballMenuOpened = false;
+				holdingBallMenu = true;
+				LoadBallColor (bowlingBall, ballMats);
+			}
+		} else if (rayHit.transform.gameObject.name == "2 YellowBall") {
+			if (controller.TriggerValue >= 0.9f) {
+				PlayerPrefs.SetInt ("ballColorInt", 2);
+				ballMenu.SetActive (false);
+				ballMenuOpened = false;
+				holdingBallMenu = true;
+				LoadBallColor (bowlingBall, ballMats);
+			}
+		} else if (rayHit.transform.gameObject.name == "3 GreenBall") {
+			if (controller.TriggerValue >= 0.9f) {
+				PlayerPrefs.SetInt ("ballColorInt", 3);
+				ballMenu.SetActive (false);
+				ballMenuOpened = false;
+				holdingBallMenu = true;
+				LoadBallColor (bowlingBall, ballMats);
+			}
+		} else if (rayHit.transform.gameObject.name == "4 BlueBall") {
+			if (controller.TriggerValue >= 0.9f) {
+				PlayerPrefs.SetInt ("ballColorInt", 4);
+				ballMenu.SetActive (false);
+				ballMenuOpened = false;
+				holdingBallMenu = true;
+				LoadBallColor (bowlingBall, ballMats);
+			}
+		}
 	}
 	public static void LoadBallColor (GameObject bowlingBall, Material[] ballMats) {
 		Transform ballObject = bowlingBall.transform.GetChild (0);
